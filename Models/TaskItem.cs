92df7abd09048efabab2b647114273c88a08f101
Prateek Chaplot <prateekchaplot96@gmail.com>
2023-01-02 00:02:44 +0530

    Update task item

--- Models/TaskItem.cs
+++ Models/TaskItem.cs
@@ -2,6 +2,7 @@ namespace TaskMaster.Models;
 
 public class TaskItem
 {
+    public int Id { get; set; }
     public string Description { get; set; }
     public DateTime DueDate { get; set; }
     public bool IsCompleted { get; set; }
